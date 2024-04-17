5749ce70cc43faabfc4bd943a1ef74b75d3ca770
Valen435 <ia.valentina@ufg.edu.sv>
2024-04-16 20:06:12 -0600

    actividad

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "fragmento_estatico_Pocasangre_Bonilla"
+include(":app")
+ 
